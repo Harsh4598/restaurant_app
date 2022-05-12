1da698d3ad27ceb9b95ab314428c029f77319a1a
Harsh4598 <harsh.ht008@gmail.com>
2022-05-12 10:16:49 -0400

    check sonar

---
+++ RestaurantBl/SearchUser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using RestaurantDl;
+using RestaurantModel;
+
+namespace RestaurantBl
+{
+	public class SearchUser
+	{
+		public SearchUser()
+		{
+		}
+		/// <summary>
+        /// it will ask for input to search a registered user in the database and will display the details of that user
+        /// </summary>
+		public static void SearchUserAsAdmin()
+		{
+			Console.Clear();
+			UserRepository userRepository = new UserRepository();
+			List<UserModelClass> userList = userRepository.GetItemFromDB();
+			search:
+			Console.Write("Search Users : ");
+			string? searchValue= Console.ReadLine();
+			if (searchValue == "")
+			{
+				Console.WriteLine("\nInvalid Input!!\n");
+				goto search;
+			}
+			else
+			{
+
+				var filteredUsers =from user in userList
+									where user.FirstName.ToLower().Contains(searchValue.ToLower()) ||
+										  user.LastName.ToLower().Contains(searchValue.ToLower()) ||
+										  user.EmailId.ToLower().Contains(searchValue.ToLower()) ||
+										  user.ContactNo.ToString().Contains(searchValue)
+									select user;
+				if (filteredUsers.Count()>0) {
+					foreach (var user in filteredUsers.Select((value, index) => new { value, index }))
+					{
+						Console.WriteLine("\n---------------------------\n");
+						Console.WriteLine("User: " + (user.index + 1));
+						Console.WriteLine("First Name: " + user.value.FirstName);
+						Console.WriteLine("Last Name: " + user.value.LastName);
+						Console.WriteLine("EmailId: " + user.value.EmailId);
+						Console.WriteLine("Contact NO: " + user.value.ContactNo);
+						if ((user.index + 1) == filteredUsers.Count())
+						{
+							Console.WriteLine("\n---------------------------\n");
+						}
+
+                    }
+				}
+				else
+				{
+					Console.WriteLine("\n---------------------------\n");
+					Console.WriteLine("User not Found!!");
+					Console.WriteLine("\n---------------------------\n");
+				}
+			}
+        }
+	}
+}
+
---
+++ RestaurantDl/UserRepository.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.Data.SqlClient;
+using RestaurantModel;
+using System.IO;
+
+namespace RestaurantDl
+{
+    public class UserRepository : IItemRepository<UserModelClass>
+    {
+        private const string connectionStringFilePath = "../RestaurantDl/Db-Connection-string-File.txt";
+        private readonly string connectionString;
+
+        public UserRepository()
+        {
+            
+            connectionString = File.ReadAllText(connectionStringFilePath);
+        }
+        public List<UserModelClass> GetItemFromDB()
+        {
+            string commandString = "SELECT * FROM Users";
+
+            using SqlConnection connection = new(connectionString);
+            connection.Open();
+            using SqlCommand command = new(commandString, connection);
+           
+            using SqlDataReader reader = command.ExecuteReader();
+
+            var users = new List<UserModelClass>();
+            while (reader.Read())
+            {
+                users.Add(new UserModelClass
+                {
+                    UserId = reader.GetInt32(0),
+                    FirstName = reader.GetString(1),
+                    LastName = reader.GetString(2),
+                    EmailId = reader.GetString(3),
+                    Password = reader.GetString(4),
+                    ContactNo = reader.GetDecimal(5),
+                });
+            }
+            //foreach (var user in users)
+            //{
+            //    Console.WriteLine(user.FirstName);
+            //}
+            connection.Close();
+            return users;
+        }
+
+        public string AddItemToDB(UserModelClass item)
+        {
+            
+            string result = "error";
+            string commandString = "INSERT INTO Users(FirstName,LastName,EmailId,UserPassword,ContactNo)VALUES(@fname, @lname, @email, @pass, @contact)";
+
+            using SqlConnection connection = new(connectionString);
+            try
+            {
+                
+                connection.Open();
+                using SqlCommand command = new(commandString, connection);
+                command.Parameters.AddWithValue("@fname", item.FirstName);
+                command.Parameters.AddWithValue("@lname", item.LastName);
+                command.Parameters.AddWithValue("@email", item.EmailId);
+                command.Parameters.AddWithValue("@pass", item.Password);
+                command.Parameters.AddWithValue("@contact", item.ContactNo);
+               
+               var success= command.ExecuteNonQuery();
+                Console.WriteLine(success);
+                result = "User Added!!!";
+            }
+            catch (Exception ex)
+            {
+                result = ex.Message;
+            }
+            finally
+            {
+                connection.Close();
+            }
+               
+                return result;
+        }
+
+        
+    }
+}
